485b02c0a6fb4b8e6c150e94763b1506cff71fef
Tony Park <tpark43@gmail.com>
2021-09-17 16:07:38 -0400

    implemented ssg

--- src/app.js
+++ src/app.js
@@ -1,28 +1,15 @@
 #!/usr/bin/env node
-const yargs = require('yargs');
 const chalk = require('chalk');
-const { name, version } = require('../package.json');
+const { convertFilesToHTML, getParams } = require('./util');
 
 try {
-    const { argv } = yargs
-        .usage('Usage: $0 [options]')
-        .example(`$0 -f 'Silver Blaze.txt'`)
-        .option('i', {
-            alias: 'input',
-            describe: 'Input a file or a directory',
-            type: 'string',
-            required: true
-        })
-        .alias('v', 'version')
-        .version('version', `The ${chalk.green(name)}'s version is ${chalk.green(version)}`)
-        .alias('h', 'help')
-        .help('help', `Show usage information`);
+    const { argv } = getParams();
     
-    const { input }  = argv;
-    if(!input) {
+    const { input: fileOrDirectory, output: outputDir, stylesheet: cssUrl }  = argv;
+    if(!fileOrDirectory) {
         throw new Error('Please input filename');
     }
-    console.log("My file is " + input);    
+    convertFilesToHTML(fileOrDirectory, cssUrl, outputDir);
 } catch(err) {
     console.log(chalk.red(err.message));
 }
